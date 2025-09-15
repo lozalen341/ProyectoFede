1e07db4445bd3a7020098a07cfcbc4f88fcc20ed
lozalen341 <lozaleandro54@gmail.com>
2025-09-15 14:41:24 -0300

    api

--- api/routes/continentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const contienentsController =  require('../controllers/continentController')
-
-router.get('/getall', contienentsController.getAllContinents);
-router.put('/update/:id',contienentsController.setContinents);
-router.delete('/delete/:id', contienentsController.deleteContinents);
-
-module.exports = router;
--- api/routes/countryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const countryRoutes =  require('../controllers/countryController')
-
-router.get('/getall', countryRoutes.getAllCountries);
-router.put('/update/:id',countryRoutes.setCountries);
-router.delete('/delete/:id',countryRoutes.deleteCountries);
-
-module.exports = router;
