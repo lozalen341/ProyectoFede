0110a70126c2ff767bc2b4b17551f8c0eb4a12fb
lozalen341 <148591407+lozalen341@users.noreply.github.com>
2025-09-22 02:42:35 -0300

    Delete api/routes/turnosRoutes.js

--- api/routes/turnosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const TurnosController = require('../controllers/turnosController')
-
-router.get('/getall', TurnosController.getAllTurnos)
-
-module.exports = router
